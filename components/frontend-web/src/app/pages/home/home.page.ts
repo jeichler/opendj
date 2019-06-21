d201c5b83f99ac3de4ab14ffb105475098d17edd
Ortwin Schneider <ortwin.schneider@googlemail.com>
2019-06-21 20:53:58 +0200

    Charts module for Home page. party mood barometer...

--- components/frontend-web/src/app/pages/home/home.page.ts
+++ components/frontend-web/src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ChartDataSets, ChartOptions, Color, Label } from 'chart.js';
 
 
 @Component({
@@ -10,9 +11,26 @@ import { Router } from '@angular/router';
 })
 export class HomePage implements OnInit {
 
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Party Barometer' },
+  ];
+  public lineChartLabels: Label[] = ['19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: '#efefef',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+
   constructor(
     public router: Router
   ) { }
+
 
   ngOnInit() {
   }
