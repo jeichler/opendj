c88b53e68ec259c6de1b6664988627b059808c56
Ortwin Schneider <ortwin.schneider@googlemail.com>
2019-06-21 20:52:19 +0200

    Fronted API service methods

--- components/frontend-web/src/app/models/track.ts
+++ components/frontend-web/src/app/models/track.ts
@@ -1,8 +1,24 @@
 export class Track {
     id: string;
+    name: string;
+    artist: string;
+    year: string;
+    // tslint:disable-next-line:variable-name
+    image_url; string;
     // tslint:disable-next-line:variable-name
     duration_ms: number;
+    preview: string;
+}
+
+export class TrackDTO {
+    id: string;
     name: string;
-    uri: string;
-    type: string;
+    artist: string;
+    year: string;
+    // tslint:disable-next-line:variable-name
+    image_url; string;
+    // tslint:disable-next-line:variable-name
+    duration_ms: number;
+    preview: string;
+    addedBy: string;
 }
