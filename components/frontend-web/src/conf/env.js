f723e6904dbb1f86d17effff5eb5dbc28868afbc
Ortwin Schneider <ortwin.schneider@googlemail.com>
2019-06-24 13:58:56 +0200

    edited ws URL

--- components/frontend-web/src/conf/env.js
+++ components/frontend-web/src/conf/env.js
@@ -5,7 +5,7 @@
     window.__env.enableDebug = false;
     window.__env.curatorPassword = 'test';
     window.__env.playlistMaxSize = 100;
-    window.__env.websocketUrl = 'http://localhost:3000';
+    window.__env.websocketUrl = 'http://dev.opendj.io/api/service-playlist';
 
     window.__env.SPOTIFY_PROVIDER_API = 'http://dev.opendj.io/api/provider-spotify/v1';
     window.__env.PLAYLIST_PROVIDER_API = 'http://dev.opendj.io/api/service-playlist/v1';
