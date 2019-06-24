7fb99e8f1b000e6c680fcb74b72dcee25ea016c8
Colum Bennett <cbennett@redhat.com>
2019-06-24 17:13:02 +0100

    menu styling

--- components/frontend-web/src/app/app.component.ts
+++ components/frontend-web/src/app/app.component.ts
@@ -9,7 +9,8 @@ import { EnvService } from './providers/env.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
   loggedIn = false;
