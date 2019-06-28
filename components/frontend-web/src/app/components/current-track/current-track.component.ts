9907b4117742ccf1208b3ddef06fc81e62e8f7c6
Ortwin Schneider <ortwin.schneider@googlemail.com>
2019-06-28 09:51:42 +0200

    changed ETA in current-track component

--- components/frontend-web/src/app/components/current-track/current-track.component.ts
+++ components/frontend-web/src/app/components/current-track/current-track.component.ts
@@ -16,15 +16,16 @@ export class CurrentTrackComponent implements OnInit, OnDestroy {
     }
     this.track = value;
     this.progress = this.track.progress_ms / 1000;
-    this.calculateTotalTime();
+    // this.calculateTotalTime();
     this.intervalHandle = setInterval(() => this.countdown(), 1000);
   }
 
   track;
-  timeRemaining = 0;
-  currentTime = null;
-  totalTime = null;
-  progress;
+  timeRemaining = 0; // progressbar value
+  currentTime = null; // remaining playtime
+  playingTime = null; // played time
+  totalTime = null; // total track length
+  progress; // temp var for countdown
   intervalHandle = null;
 
   constructor() { }
@@ -32,10 +33,10 @@ export class CurrentTrackComponent implements OnInit, OnDestroy {
   calculateTotalTime() {
     const duration = this.track.duration_ms / 1000;
     let s = duration % 60;
-    let m = Math.floor(duration / 60) % 60;
+    const m = Math.floor(duration / 60) % 60;
     s = Math.round(s);
-    let sStr = s < 10 ? '0' + s : ''+s;
-    let mStr = m < 10 ? '0' + m :''+ m;
+    const sStr = s < 10 ? '0' + s : '' + s;
+    const mStr = m < 10 ? '0' + m : '' + m;
     this.totalTime = mStr + ':' + sStr;
   }
 
@@ -43,14 +44,22 @@ export class CurrentTrackComponent implements OnInit, OnDestroy {
     const duration = this.track.duration_ms / 1000;
     this.progress = this.progress + 1;
     const timeLeft = duration - this.progress;
+
     this.timeRemaining = 1 - (timeLeft / duration);
-    // console.log(1 - (timeLeft / duration));
+
     let s = timeLeft % 60;
     s = Math.round(s);
-    let m = Math.floor(timeLeft / 60) % 60;
-    let sStr = s < 10 ? '0' + s : ''+s;
-    let mStr = m < 10 ? '0' + m : ''+m;
+    const m = Math.floor(timeLeft / 60) % 60;
+    const sStr = s < 10 ? '0' + s : '' + s;
+    const mStr = m < 10 ? '0' + m : '' + m;
     this.currentTime = mStr + ':' + sStr;
+
+    const mP = Math.floor(this.progress / 60);
+    let sP = this.progress % 60;
+    sP = s = Math.round(sP);
+    const spStr = sP < 10 ? '0' + sP : '' + sP;
+    const mpStr = mP < 10 ? '0' + mP : '' + mP;
+    this.playingTime = mpStr + ':' + spStr;
   }
 
   ngOnInit() {
