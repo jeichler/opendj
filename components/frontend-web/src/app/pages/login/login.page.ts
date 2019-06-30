94ba7f34b28bef6ad16ccb3a7926ff89193318e2
Daniel Fröhlich <daniel.froehlich@redhat.com>
2019-06-30 15:32:22 +0200

    Login with Animals #57

--- components/frontend-web/src/app/pages/login/login.page.ts
+++ components/frontend-web/src/app/pages/login/login.page.ts
@@ -7,6 +7,7 @@ import { PopoverController } from '@ionic/angular';
 import { MoreOptionsComponent } from '../../components/more-options/more-options.component';
 const ung = require('username-generator');
 
+const listOfAnimals = ["Alligator","Anteater","Armadillo","Auroch","Axolotl","Badger","Bat","Bear","Beaver","Blobfish","Buffalo","Camel","Capybara","Chameleon","Cheetah","Chinchilla","Chipmunk","Chupacabra","Cormorant","Coyote","Crow","Dingo","Dinosaur","Dog","Dolphin","Duck","Dumbo Octopus","Elephant","Ferret","Fox","Frog","Giraffe","Goose","Gopher","Grizzly","Hamster","Hedgehog","Hippo","Hyena","Ibex","Ifrit","Iguana","Jackal","Jackalope","Kangaroo","Kiwi","Koala","Kraken","Lemur","Leopard","Liger","Lion","Llama","Loris","Manatee","Mink","Monkey","Moose","Narwhal","Nyan Cat","Orangutan","Otter","Panda","Penguin","Platypus","Pumpkin","Python","Quagga","Quokka","Rabbit","Raccoon","Rhino","Sheep","Shrew","Skunk","Squirrel","Tiger","Turtle","Unicorn","Walrus","Wolf","Wolverine","Wombat"];
 
 @Component({
   selector: 'app-login',
@@ -46,9 +47,16 @@ export class LoginPage implements OnInit {
     return await popover.present();
   }
 
+  userNameGeneratorForZoe() {
+    var animal = listOfAnimals[Math.floor(Math.random()*listOfAnimals.length)];
+    var number = Math.floor(Math.random()*10)+1;
+    return "Anon"+animal+number;
+  }
+
   generateUsername() {
     this.loginForm.patchValue({
-      username: ung.generateUsername()
+//      username: ung.generateUsername()
+      username: this.userNameGeneratorForZoe()
     });
   }
 
