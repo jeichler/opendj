89ec59552882a374586277c3875d4495b7f5fd7d
Justin Davis <justinndavis@gmail.com>
2019-09-26 13:40:28 +0200

    model-service test script

--- components/model-service/src/app/views.py
+++ components/model-service/src/app/views.py
@@ -2,13 +2,14 @@ from flask import render_template, flash, redirect
 from app import app
 import urllib
 import jsonify
-
+from flask import request
 from urllib.error import HTTPError
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    content = request.get_json(silent=True)
-    return content
+    req = request.get_json(silent=True)
+    print(req)
+    return req
 
 
 @app.route('/health', methods=['GET'])
