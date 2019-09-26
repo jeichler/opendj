4edeeec938a96453b7e38e83964f18b02623eac8
Justin Davis <justinndavis@gmail.com>
2019-09-26 12:42:04 +0200

    model-service update

--- components/model-service/bin/deploy.sh
+++ components/model-service/bin/deploy.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-export IP=ocp.datr.eu
-export USER=justin
-export APP=visionbot
-export PROJECT=${APP}
 
+export APP=model-service
 
-oc login ${IP}:8443 -u ${USER}
-
-oc delete project $PROJECT
-oc new-project $PROJECT 2> /dev/null
-while [ $? \> 0 ]; do
-    sleep 1
-    printf "."
-oc new-project $PROJECT 2> /dev/null
-done
+oc login https://master.ocp1.stormshift.coe.muc.redhat.com:8443 --token=3wb_CWwtDKJBQ78dSTXSUOCDr691WqDpwlrw9NzmMbU
 
 oc new-app -f deploy-template.yaml \
     -p APP_NAME=${APP} \
-    -p GIT_URI=https://github.com/justindav1s/ai-on-openshift.git \
-    -p GIT_CONTEXT_DIR=5_visionbot/src
+    -p GIT_URI=https://github.com/opendj/opendj.git \
+    -p GIT_CONTEXT_DIR=components/model-service/src
 
 oc start-build ${APP} --follow
