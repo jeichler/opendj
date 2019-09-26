219f25aadd5a7c10a3f2378971f1c78eb0ef6337
Justin Davis <justinndavis@gmail.com>
2019-09-26 12:48:01 +0200

    model-service update

--- components/model-service/bin/deploy.sh
+++ components/model-service/bin/deploy.sh
@@ -2,7 +2,7 @@
 
 export APP=model-service
 
-oc login https://master.ocp1.stormshift.coe.muc.redhat.com:8443 --token=3wb_CWwtDKJBQ78dSTXSUOCDr691WqDpwlrw9NzmMbU
+oc login https://master.ocp1.stormshift.coe.muc.redhat.com:8443 --token=${RHTE_SECRET}
 
 oc project dfroehli-opendj-dev
 
